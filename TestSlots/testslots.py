db6fa048d84d287c64a7a62da95225f6922e9c76
BTNGaming <35198559+BTNGaming@users.noreply.github.com>
2023-02-05 13:24:46 -0800

    Update testslots.py

--- TestSlots/testslots.py
+++ TestSlots/testslots.py
@@ -1,4 +1,8 @@
 from redbot.core import commands
+import discord
+from discord.ext import commands
+import asyncio
+import random
 
 class TestSlots(commands.Cog):
     """My custom animated slots cog"""
@@ -6,8 +10,23 @@ class TestSlots(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command()
+    @commands.command(name='aslots')
     async def aslot(self, ctx):
         """This does stuff!"""
         # Your code will go here
-        await ctx.send("I can do stuff!")
+        emojis = ['🍒', '🍊', '🍇', '🍉', '💰']
+        spinning_emojis = ['⠀', '⠁', '⠂', '⠃', '⠄', '⠅', '⠆', '⠇']
+        spinning = [f"{emoji} {emoji} {emoji}" for emoji in spinning_emojis]
+        result = [random.choice(emojis) for i in range(3)]
+        for spin in spinning:
+            await ctx.send(spin)
+            await asyncio.sleep(0.5)
+        await ctx.send(' '.join(result))
+        if result[0] == result[1] == result[2]:
+            await ctx.send("Congratulations! You've hit the jackpot!")
+        else:
+            await ctx.send("Better luck next time.")
+
+
+def setup(bot):
+    bot.add_cog(TestSlots(bot))
