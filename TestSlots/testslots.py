7044a2d3fc9a9ec2eb463d5117b7149d73d27e73
BTNGaming <35198559+BTNGaming@users.noreply.github.com>
2023-02-05 17:01:13 -0800

    Update testslots.py

--- TestSlots/testslots.py
+++ TestSlots/testslots.py
@@ -3,25 +3,6 @@ import asyncio
 import random
 import discord
 
-# class TestSlots(commands.Cog):
-#     def __init__(self, bot):
-#         self.bot = bot
-    
-#     @commands.command()
-#     async def aslot(self, ctx):
-#         emojis = ['\N{CHECK MARK}', '\N{X}', '🍇', '🍉', '💰']
-#         spinning_emojis = ['⠀', '⠁', '⠂', '⠃', '⠄', '⠅', '⠆', '⠇']
-#         spinning = [f"{emoji} {emoji} {emoji}" for emoji in spinning_emojis]
-#         result = [random.choice(emojis) for i in range(3)]
-#         for spin in spinning:
-#             await ctx.send(spin)
-#             await asyncio.sleep(0.5)
-#         await ctx.send(' '.join(result))
-#         if result[0] == result[1] == result[2]:
-#             await ctx.send("Congratulations! You've hit the jackpot!")
-#         else:
-#             await ctx.send("Better luck next time.")
-
 class TestSlots(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
@@ -50,7 +31,7 @@ class TestSlots(commands.Cog):
         message = await ctx.send(embed=embed)
 
         for i in range(8):
-            embed.description = " ".join([random.choice(spinning_emojis) for _ in range(3)])
+            embed.description = " ".join([reel1 | reel2 | reel3 for _ in range(3)])
             await message.edit(embed=embed)
             await asyncio.sleep(0.5)
 
