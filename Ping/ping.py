8049e2d72a610387101ed1df0a46221dfa94c1ac
BTNGaming <35198559+BTNGaming@users.noreply.github.com>
2023-02-05 19:56:04 -0800

    Update ping.py

--- Ping/ping.py
+++ Ping/ping.py
@@ -1,12 +1,27 @@
-from redbot.core import commands
+import discord
 
-class Ping(commands.Cog):
-    """A simple ping command."""
+client = discord.Client()
 
-    def __init__(self, bot):
-        self.bot = bot
+@client.event
+async def on_ready():
+    print(f"Bot is ready. Login as {client.user}")
 
-    @commands.command(name="ping2", aliases=["p"])
-    async def _ping(self, ctx):
-        """Replies with 'Pong!'"""
-        await ctx.send("Pong!")
+@client.event
+async def on_message(message):
+    if message.content.startswith("/mycommand"):
+        # Your code here
+        await message.channel.send("Your response here")
+
+
+# from redbot.core import commands
+
+# class Ping(commands.Cog):
+#     """A simple ping command."""
+
+#     def __init__(self, bot):
+#         self.bot = bot
+
+#     @commands.command(name="ping2", aliases=["p"])
+#     async def _ping(self, ctx):
+#         """Replies with 'Pong!'"""
+#         await ctx.send("Pong!")
