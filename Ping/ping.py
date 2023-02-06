c00e163d5c70906129db6d37c518ac38eaec41fa
BTNGaming <35198559+BTNGaming@users.noreply.github.com>
2023-02-05 19:50:20 -0800

    Update ping.py

--- Ping/ping.py
+++ Ping/ping.py
@@ -3,6 +3,9 @@ from redbot.core import commands
 class Ping(commands.Cog):
     """A simple ping command."""
 
+    def __init__(self, bot):
+        self.bot = bot
+
     @commands.command(name="ping", aliases=["p"])
     async def _ping(self, ctx):
         """Replies with 'Pong!'"""
